7f0307ff6a40082777a5cc9b24ced9335efdcffc
Sveta C <svetlana.c@quali.com>
2022-11-13 19:30:04 +0200

    added tags expression

--- s3-write/main.tf
+++ s3-write/main.tf
@@ -2,8 +2,22 @@ provider "aws" {
   region = "eu-west-1"
 }
 
+locals {
+  common_tags = {
+    creator        = "terraform"
+  }
+}
+
 resource "aws_s3_bucket_object" "object" {
   bucket  = "${var.BUCKET_NAME}"
   key     = "${var.SANDBOX_ID}.json"
   content = "{ \"Content\": \"${var.CONTENT}\"}"
+  
+  tags = merge(
+    local.common_tags,
+    {
+      custom_tag_1 = "val1"
+      "custom_tag_2" = "val2"
+    }
+  )
 }
