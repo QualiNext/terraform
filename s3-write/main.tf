d80d00feaa75c5ea4451fdff7d72acaa400bf84d
Sveta C <svetlana.c@quali.com>
2022-11-16 19:36:32 +0200

    content heredoc

--- s3-write/main.tf
+++ s3-write/main.tf
@@ -13,7 +13,10 @@ locals {
 resource "aws_s3_bucket_object" "object" {
   bucket  = "${var.BUCKET_NAME}"
   key     = "${var.SANDBOX_ID}.json"
-  content = "{ \"Content\": \"${var.CONTENT}\"}"
+  #content = "{ \"Content\": \"${var.CONTENT}\"}"
+  content = <<EOF
+Content: ${var.CONTENT}
+EOF
   
   tags = merge(
     local.common_tags,
